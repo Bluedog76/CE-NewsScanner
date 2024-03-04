b66891647c3510b57065217b7a41d0627ff07735
Bluedog76 <52477795+Bluedog76@users.noreply.github.com>
2024-03-04 16:36:37 +0000

    JSON implement start

--- index.js
+++ index.js
@@ -54,7 +54,18 @@ function GetAscii() {
 }
 
 function Getdata() {
-
+  fs.readFile("./Config/config.json", "utf8", (err, jsonString) => {
+  if (err) {
+    console.log("Error reading file from disk:", err);
+    return;
+  }
+  try {
+    const customer = JSON.parse(jsonString);
+    console.log("Customer address is:", customer.address); // => "Customer address is: Infinity Loop Drive"
+  } catch (err) {
+    console.log("Error parsing JSON string:", err);
+  }
+});
 }
 
 //^I dont know how the fuck this is working so dont fucking touch it.
