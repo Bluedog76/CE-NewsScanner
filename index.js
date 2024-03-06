34b269eca219b158651a715e423ea37d15231f09
Bluedog76 <52477795+Bluedog76@users.noreply.github.com>
2024-03-06 20:32:50 +0000

    Cleaned code, better token help message, general debuging

--- index.js
+++ index.js
@@ -2,11 +2,14 @@
 require('dotenv').config();
 const Token = process.env.TOKEN;
 if (Token == undefined) {
-  console.log("SAFE ABORT")
-  console.log("There is no active tooken set in the .env config file. Please add a token...")
+  console.log('\x1b[41m',"SAFE ABORT ")
+  console.log("There is no active token set in the .env config file. Please add a token...")
+  console.log("You can do so by creating a .env file in the root of the aplication")
+  console.log("the open and type and replace <your data> w/ your token: ")
+  console.log('\x1b[36m%s\x1b[0m', "TOKEN=<your data>")
   process.exit();
 }
-console.log("Loaded Config & Set");
+console.log("Token Loaded");
 
 
 //promting
@@ -18,7 +21,7 @@ const fs = require('fs');
 
 //load NewAPI
 const NewsAPI = require('newsapi');
-const { exit } = require('process');
+//const { exit } = require('process');
 //const { exit } = require('process');
 const newsapi = new NewsAPI(Token);
 
@@ -34,16 +37,7 @@ function GetDate() {
 
 
 // file system https://tutorialedge.net/nodejs/reading-writing-files-with-nodejs/
-function GetConfig() {
-  var fun = fs.readFileSync("config.txt", "utf-8", (err, data) => {
-    if (err) throw err;
-    console.log(data);
-    return data;
-  })
-  return fun;
 
-
-}
 function GetAscii() {
   var fun = fs.readFileSync("./ExtraFiles/ascii_husky_small.txt", "utf-8", (err, data) => {
     if (err) throw err;
@@ -56,37 +50,43 @@ function GetAscii() {
 function Getdata() {
   fs.readFile("./Config/config.json", "utf8", (err, jsonString) => {
   if (err) {
-    console.log("Error reading file from disk:", err);
+    console.log("Error reading file", err);
     return;
   }
   try {
-    const customer = JSON.parse(jsonString);
-    console.log("Customer address is:", customer.address); // => "Customer address is: Infinity Loop Drive"
+    const presets = JSON.parse(jsonString);
+    console.log(presets);
+    return presets
   } catch (err) {
     console.log("Error parsing JSON string:", err);
+    return;
   }
 });
 }
-
 //^I dont know how the fuck this is working so dont fucking touch it.
 
 var Today = GetDate()
-var configdecript = GetConfig()
 var asciihusky = GetAscii()
 console.log(asciihusky);
 console.log("Succesfully loaded Functions, Packages, and Config on the "+ Today);
-console.log("Loading...");
 
-console.log("What Preset Would you like to Chose")
-console.log("Preset1")
-console.log("Preset2")
-console.log("Preset3")
-console.log("Preset4") 
+
+var jsonout = Getdata()
+
+var Preset1 = "ye"
+
+
+//starts the user prompting
+console.log("What Preset Would you like to Chose?")
+console.log("1. " + Preset1)
+console.log("2. " + "Preset2")
+console.log("3. " + "Preset3")
+console.log("4. " + "Preset4") 
 
 
 //get preset
 function Preset() {
-  var answer = prompt("> ")
+  var answer = prompt("Input: ")
   var passed = 0;
 
   if (answer == 1) {
@@ -103,8 +103,9 @@ function Preset() {
   }
   return answer;
 } 
-Preset();
 
+var SelectedPreset = Preset();
+//gets data from users --Start important suff here 
 
 
 
